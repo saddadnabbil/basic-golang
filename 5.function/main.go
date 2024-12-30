975b4c95b9100724be2303a93c92567febe0692c
saddadnabbil <saddadnabbil@gmail.com>
2024-12-30 20:31:57 +0700

    learn: function (multiple return value)

--- 5.function/main.go
+++ 5.function/main.go
@@ -6,8 +6,16 @@ func greet(name string) string {
 	return "Hello " + name
 }
 
+func calculate(a, b int) (int, int) {
+	return a + b, a * b
+}
+
 func main() {
 	// function
 	message := greet("Nabbil")
 	fmt.Println(message)
+
+	// multiple return values
+	sum, product := calculate(2, 3)
+	fmt.Println("Sum: ", sum, "Product: ", product)
 }
